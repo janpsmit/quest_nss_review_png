e5a3d56f24ffcf959683e976dd88e74cb22eec45
Jan Smit <smit1@un.org>
2026-05-05 13:21:24 +0700

    Enable TOC and progress bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Survey } from "survey-react-ui";
 import { Model } from "survey-core";
 import "./styles/survey.css";
+import intro from "./survey/intro_guidance.json";
 import legalFramework from "./survey/part1_legal_framework.json";
 import subjectAreas from "./survey/part2_subject_areas.json";
 import part3Documents from "./survey/part3_supporting_documents.json";
@@ -10,12 +11,18 @@ export default function App() {
   const survey = new Model({
     title: "Global Assessment – Self-Assessment Questionnaire",
     pages: [
+      ...intro.pages,
       ...legalFramework.pages,
       ...subjectAreas.pages,
       ...part3Documents.pages
     ]
   });
 
+survey.showTOC = true;
+survey.tocLocation = "left";
+survey.showProgressBar = "top";
+survey.progressBarType = "pages";
+  
   survey.onComplete.add((sender) => {
     console.log("Survey results:", sender.data);
   });
