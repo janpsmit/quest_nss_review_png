59d038a0bf0583b8ce0b2345426ea24449689335
Jan Smit <smit1@un.org>
2026-05-26 09:08:09 +0700

    Enable CORS for frontend

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
+import cors from "cors";
 import express from "express";
 import fs from "fs";
 import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
